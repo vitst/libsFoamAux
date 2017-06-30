7709b9173965e9e0dff4db5a74c9e28dde0c8cbe
tonyladd <tladd@che.ufl.edu>
2017-06-30 11:13:37 -0400

    Revert "Commented out test for 0 time directory"
    This reverts commit e0e84117c3c4e58d9e46f9866b0f25217172dbff.
    
    Commited to the wrong branch

--- dissolMotion/dissolMotionPointPatchVectorField.C
+++ dissolMotion/dissolMotionPointPatchVectorField.C
@@ -1579,7 +1579,6 @@ make_lists_and_normals()
     timeTmp.setTime(timeDirs[1], 0);
   }
   
-/*
   // in case 0 time does not exist
   if( timeTmp.timeName()!="0" ){
     SeriousErrorIn("meshRelax")
@@ -1587,7 +1586,6 @@ make_lists_and_normals()
             "If you run in parallel please check a decomposition."
             <<exit(FatalError);
   }
-*/
   
   Foam::fvMesh meshTmp
   (
@@ -1782,7 +1780,6 @@ calc_weights_surface()
     timeTmp.setTime(timeDirs[1], 0);
   }
   
-/*
   // in case 0 time does not exist
   if( timeTmp.timeName()!="0" ){
     SeriousErrorIn("meshRelax")
@@ -1790,7 +1787,6 @@ calc_weights_surface()
             "If you run in parallel please check a decomposition."
             <<exit(FatalError);
   }
-*/
   
   Foam::fvMesh meshTmp
   (

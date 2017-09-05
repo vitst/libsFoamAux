28c499ab8efacde6eeed16282d23bdc656a15829
Vitaliy Starchenko <vitaliy.starchenko@gmail.com>
2017-09-05 13:58:42 -0400

    removed commented parts

--- normalMotionSlipFvPatchField/normalMotionSlipFvPatchVectorField.C
+++ normalMotionSlipFvPatchField/normalMotionSlipFvPatchVectorField.C
@@ -25,25 +25,18 @@ License
 
 #include "normalMotionSlipFvPatchVectorField.H"
 #include "volFields.H"
-#include "surfaceFields.H"
-#include "fvcMeshPhi.H"
 #include "symmTransformField.H"
 #include "addToRunTimeSelectionTable.H"
-#include "pointFields.H"
-
-#include "coupledPatchInterpolation.H"
-
 
 #include "normalMotionSlipPointPatchVectorField.H"
-
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
 Foam::normalMotionSlipFvPatchVectorField::
 normalMotionSlipFvPatchVectorField
 (
-    const fvPatch& p,
-    const DimensionedField<vector, volMesh>& iF
+  const fvPatch& p,
+  const DimensionedField<vector, volMesh>& iF
 )
 :
     fixedValueFvPatchVectorField(p, iF)
@@ -58,257 +51,95 @@ normalMotionSlipFvPatchVectorField
     const dictionary& dict
 )
 :
-    fixedValueFvPatchVectorField(p, iF)
+  fixedValueFvPatchVectorField(p, iF)
 {
-    fvPatchVectorField::operator=(vectorField("value", dict, p.size()));
+  fvPatchVectorField::operator=(vectorField("value", dict, p.size()));
 }
-
 
 Foam::normalMotionSlipFvPatchVectorField::
 normalMotionSlipFvPatchVectorField
 (
-    const normalMotionSlipFvPatchVectorField& ptf,
-    const fvPatch& p,
-    const DimensionedField<vector, volMesh>& iF,
-    const fvPatchFieldMapper& mapper
+  const normalMotionSlipFvPatchVectorField& ptf,
+  const fvPatch& p,
+  const DimensionedField<vector, volMesh>& iF,
+  const fvPatchFieldMapper& mapper
 )
 :
     fixedValueFvPatchVectorField(ptf, p, iF, mapper)
 {}
 
-
 Foam::normalMotionSlipFvPatchVectorField::
 normalMotionSlipFvPatchVectorField
 (
-    const normalMotionSlipFvPatchVectorField& mwvpvf
+  const normalMotionSlipFvPatchVectorField& mwvpvf
 )
 :
     fixedValueFvPatchVectorField(mwvpvf)
 {}
 
-
 Foam::normalMotionSlipFvPatchVectorField::
 normalMotionSlipFvPatchVectorField
 (
-    const normalMotionSlipFvPatchVectorField& mwvpvf,
-    const DimensionedField<vector, volMesh>& iF
+  const normalMotionSlipFvPatchVectorField& mwvpvf,
+  const DimensionedField<vector, volMesh>& iF
 )
 :
-    fixedValueFvPatchVectorField(mwvpvf, iF)
+  fixedValueFvPatchVectorField(mwvpvf, iF)
 {}
-
 
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
   
-void Foam::normalMotionSlipFvPatchVectorField::evaluate
-(
-    const Pstream::commsTypes commsType
-)
-{
-  //Info<<"   normalMotionSlipFvPatchVectorField::evaluate"<<nl;
-  
-  fixedValueFvPatchField<vector>::evaluate(commsType);
-}
-
-void Foam::normalMotionSlipFvPatchVectorField::initEvaluate
-(
-    const Pstream::commsTypes commsType
-)
-{
-  //Info<<"   normalMotionSlipFvPatchVectorField::initEvaluate"<<nl;
-  
-  fixedValueFvPatchField<vector>::initEvaluate(commsType);
-}
-
-
 void Foam::normalMotionSlipFvPatchVectorField::updateCoeffs()
 {
-  //Info<<"   normalMotionSlipFvPatchVectorField::updateCoeffs"<<nl;
-  /*
-    const fvPatch& p = this->patch();
-    const polyPatch& pp = p.patch();
-    const fvMesh& mesh = this->internalField().mesh();
-    const pointField& points = mesh.points();
-
-    word pfName = this->internalField().name();
-    pfName.replace("cell", "point");
-
-    const GeometricField<vector, pointPatchField, pointMesh>& pointMotion =
-        this->db().objectRegistry::template
-            lookupObject<GeometricField<vector, pointPatchField, pointMesh>>
-            (pfName);
-
-    forAll(p, i)
-    {
-        this->operator[](i) = pp[i].average(points, pointMotion);
-    }
-    
-    
-    vectorField pif = this->patchInternalField();
-    Info<<"FVFV: pif: "<< max(pif) <<nl;
-
-    fixedValueFvPatchField<vector>::updateCoeffs();
-   */
+  if(debug) 
+  {
+    Info<<"   normalMotionSlipFvPatchVectorField::updateCoeffs"<<nl;
+  }
   
-    if (updated())
-    {
-        return;
-    }
-  
-    const fvMesh& mesh = internalField().mesh();
+  if (updated())
+  {
+      return;
+  }
 
-      const fvPatch& p = patch();
-        const polyPatch& pp = p.patch();
-        const pointField& oldPoints = mesh.oldPoints();
+  const vectorField& n = this->patch().nf();
+  const label& patchID = this->patch().index();
 
-        vectorField oldFc(pp.size());
+  const pointVectorField& pmU = 
+          this->db().objectRegistry::lookupObject<pointVectorField>
+          (
+            "pointMotionU"
+          );
 
-        forAll(oldFc, i)
-        {
-            oldFc[i] = pp[i].centre(oldPoints);
-        }
-
-        const scalar deltaT = mesh.time().deltaTValue();
-
-        const vectorField Up((pp.faceCentres() - oldFc)/deltaT);  // lg: field of wall velocity
-
-        //const volVectorField& U =
-        //    static_cast<const volVectorField&>(internalField()); // lg: field of given vector in U input file
-        
-        //const volVectorField& U =
-        //  this->db().objectRegistry::lookupObject<volVectorField>("U");
-        const volScalarField& C =
-          this->db().objectRegistry::lookupObject<volScalarField>("C");
-
-        /*
-        scalarField phip
-        (
-            p.patchField<surfaceScalarField, scalar>(fvc::meshPhi(U))
-        );
-        const vectorField n(p.nf()); // lg: assuming this is unit surface normal field of patch
-        const scalarField& magSf = p.magSf();
-        tmp<scalarField> Un = phip/(magSf + VSMALL);
-        */
-        
-        const vectorField nHat(this->patch().nf());
-        label patchID = this->patch().index();
-        
-        //Info<<"      iniDisp: "<<iniDisp<<nl;
-        //if(iniDisp)
-        /*
-        {
-          const IOdictionary& IOd
-                = this->db().lookupObject<IOdictionary>("transportProperties");
-          scalar lR =  (new dimensionedScalar(IOd.lookup("lR")))->value();
-          scalarField grC = -C.boundaryField()[patchID].snGrad();
-
-          displacement = lR * grC * nHat;
-          //iniDisp = false;
-        }
-         */
-        const pointVectorField& pvf = 
-          this->db().objectRegistry::lookupObject<pointVectorField>("pointMotionU");
-        
-        const Foam::normalMotionSlipPointPatchVectorField& pvfBC =
-                refCast<const Foam::normalMotionSlipPointPatchVectorField>
-                ( (pvf.boundaryField()[patchID]) );
-        
-        
-        vectorField disp2 =
-            transform(I - sqr(nHat), this->patchInternalField());
-        
-        vectorField disp(pp.size());
-        /*
-        vectorField newNFc(pp.size());
-        
-        Info<<"      iniDisp: "<<iniDisp<<nl;
-        if(iniDisp)
-        {
-            //const fvMesh& fvmesh_ = refCast<const fvMesh>(mesh);
-            coupledPatchInterpolation patchInterpolator
-            ( 
-              mesh.boundaryMesh()[patchID], mesh
-            );
-            scalarField motionC=patchInterpolator.faceToPointInterpolate(grC);
-            vectorField motionN=patchInterpolator.faceToPointInterpolate(nHat);
-            // normalize point normals to 1
-            forAll(motionN, ii) motionN[ii]/=mag(motionN[ii]);
-            vectorField pointMotion = lR * motionC * motionN;
-            
-            pointField newP = mesh.boundaryMesh()[patchID].localPoints() + pointMotion;
+  const Foam::normalMotionSlipPointPatchVectorField& pmuBC =
+          refCast<const Foam::normalMotionSlipPointPatchVectorField>
+          (
+            pmU.boundaryField()[patchID]
+          );
 
 
-            forAll(oldFc, i)
-            {
-                newNFc[i] = pp[i].normal(newP);
-            }
-          
-            disp1 = lR * grC * nHat;
-        }
-        else
-        {
-          disp1 = this->patchInternalField();
-        }
-        vectorField disp2 =
-            transform(I - sqr(newNFc), this->patchInternalField());
+  vectorField::operator=
+  ( 
+    pmuBC.getDisp() + transform(I - sqr(n), this->patchInternalField())
+  );
 
-        iniDisp=false;
-
-        */
-        
-        disp = pvfBC.getDisp() + disp2;
-        
-        vectorField::operator=
-        ( 
-          disp
-        );
-        
-        //vectorField::operator=(nHat*(nHat &  (Up + n*(Un - (n & Up))) )
-		//+ transform(I - sqr(nHat), this->patchInternalField()) );
-        /*
-        vectorField::operator=
-        ( 
-            this->patchInternalField()
-            + 
-            transform(I - sqr(nHat), this->patchInternalField()) 
-        );
-         */
-        
-        //forAll(p, i)
-        //{
-        //    this->operator[](i) = disp[i];
-        //}
-        
-        
-        /*
-        vectorField pif = this->patchInternalField();
-        Info<<"FVFV: pif:    "<< max(pif) << nl
-            << "      disp:   "<<max(disp) << nl
-            << "      disp1:  "<<max(disp1) <<nl
-            << "      disp2:  "<<max(disp2) <<nl;
-         */
-        
-
-    
-    fixedValueFvPatchVectorField::updateCoeffs();
+  fixedValueFvPatchVectorField::updateCoeffs();
 }
 
 void Foam::normalMotionSlipFvPatchVectorField::write(Ostream& os) const
 {
-    fvPatchVectorField::write(os);
-    writeEntry("value", os);
+  fvPatchVectorField::write(os);
+  writeEntry("value", os);
 }
-
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
 namespace Foam
 {
-    makePatchTypeField
-    (
-        fvPatchVectorField,
-        normalMotionSlipFvPatchVectorField
-    );
+  makePatchTypeField
+  (
+    fvPatchVectorField,
+    normalMotionSlipFvPatchVectorField
+  );
 }
 
 // ************************************************************************* //
--- normalMotionSlipPointPatchField/normalMotionSlipPointPatchVectorField.C
+++ normalMotionSlipPointPatchField/normalMotionSlipPointPatchVectorField.C
@@ -42,52 +42,40 @@ Foam::normalMotionSlipPointPatchVectorField::normalMotionSlipPointPatchVectorFie
     const DimensionedField<vector, pointMesh>& iF
 )
 :
-//    slipPointPatchField<vector>(p, iF)
-//    fixedValuePointPatchField<vector>(p, iF)
-    valuePointPatchField<vector>(p, iF)
-//    calculatedPointPatchField<vector>(p, iF)
+  valuePointPatchField<vector>(p, iF)
 {}
 
 
 Foam::normalMotionSlipPointPatchVectorField::normalMotionSlipPointPatchVectorField
 (
-    const pointPatch& p,
-    const DimensionedField<vector, pointMesh>& iF,
-    const dictionary& dict
+  const pointPatch& p,
+  const DimensionedField<vector, pointMesh>& iF,
+  const dictionary& dict
 )
 :
-//    slipPointPatchField<vector>(p, iF, dict)
-//    fixedValuePointPatchField<vector>(p, iF, dict)
-    valuePointPatchField<vector>(p, iF, dict)
-//    calculatedPointPatchField<vector>(p, iF, dict)
+  valuePointPatchField<vector>(p, iF, dict)
 {}
 
 
 Foam::normalMotionSlipPointPatchVectorField::normalMotionSlipPointPatchVectorField
 (
-    const normalMotionSlipPointPatchVectorField& ptf,
-    const pointPatch& p,
-    const DimensionedField<vector, pointMesh>& iF,
-    const pointPatchFieldMapper& mapper
+  const normalMotionSlipPointPatchVectorField& ptf,
+  const pointPatch& p,
+  const DimensionedField<vector, pointMesh>& iF,
+  const pointPatchFieldMapper& mapper
 )
 :
-//    slipPointPatchField<vector>(ptf, p, iF, mapper)
-//    fixedValuePointPatchField<vector>(ptf, p, iF, mapper)
     valuePointPatchField<vector>(ptf, p, iF, mapper)
-//    calculatedPointPatchField<vector>(ptf, p, iF, mapper)
 {}
 
 
 Foam::normalMotionSlipPointPatchVectorField::normalMotionSlipPointPatchVectorField
 (
-    const normalMotionSlipPointPatchVectorField& ptf,
-    const DimensionedField<vector, pointMesh>& iF
+  const normalMotionSlipPointPatchVectorField& ptf,
+  const DimensionedField<vector, pointMesh>& iF
 )
 :
-//    slipPointPatchField<vector>(ptf, iF)
-//    fixedValuePointPatchField<vector>(ptf, iF)
-    valuePointPatchField<vector>(ptf, iF)
-//    calculatedPointPatchField<vector>(ptf, iF)
+  valuePointPatchField<vector>(ptf, iF)
 {}
 
 
@@ -98,64 +86,42 @@ void Foam::normalMotionSlipPointPatchVectorField::evaluate
     const Pstream::commsTypes
 )
 {
-  //Info<<"normalMotionSlipPointPatchVectorField::evaluate"<<nl;
+  if(debug) 
+  {
+    Info<<"   normalMotionSlipPointPatchVectorField::evaluate"<<nl;
+  }
   
   const polyMesh& mesh = this->internalField().mesh()();
-        label patchID = this->patch().index();
+  const label& patchID = this->patch().index();
   const fvMesh& fvmesh_ = refCast<const fvMesh>(mesh);
   coupledPatchInterpolation patchInterpolator
   ( 
     mesh.boundaryMesh()[patchID], fvmesh_
   );
   
-    Field<vector>& iF = const_cast<Field<vector>&>(this->primitiveField());
-
-        const volVectorField& cmu =
-          this->db().objectRegistry::lookupObject<volVectorField>("cellMotionU");
-        vectorField cmuB = cmu.boundaryField()[patchID];
-		vectorField pm = patchInterpolator.faceToPointInterpolate( cmuB );
-        
-        vectorField pif = this->patchInternalField();
-  vectorField pNf = mesh.boundaryMesh()[patchID].faceNormals();
+  const volVectorField& cmu =
+    this->db().objectRegistry::lookupObject<volVectorField>("cellMotionU");
   
-    vectorField tvalues =
-        ( 
-            pm
-            /*
-						pif
-            + 
-            transform(I - pNf*pNf, pm) 
-             */
-        );
-    
-    //Info<< "Set iniDisp in normalMotionSlipFvPatchVectorField"<<nl;
-    //cmu.boundaryFieldRef()[patchID].setIniDisp(true);
-    
-	//	Info<<"EV poMo: "<<max(pif)<<"  f0 "<<max(pm)<<"    tval: "<<max(tvalues)<<nl;
-    //this->operator==( this->primitiveField() );
-    //this->operator==( pointMotion );
-    //this->operator==( pm );
-    //this->operator==( tvalues() );
-    this->operator==( tvalues );
-    //this->setInInternalField(iF, tvalues);
-    
-	//	Info<<"IIInternField: "<<max(this->patchInternalField())<<nl;
+  this->operator==
+  ( 
+    patchInterpolator.faceToPointInterpolate( cmu.boundaryField()[patchID] ) 
+  );
   
   valuePointPatchField<vector>::evaluate();
-  //fixedValuePointPatchField<vector>::evaluate();
-  //calculatedPointPatchField<vector>::evaluate();
 }
 
 void Foam::normalMotionSlipPointPatchVectorField::updateCoeffs()
 {
-  //Info<<"normalMotionSlipPointPatchVectorField::updateCoeffs"<<nl;
-  //iniDisp = true;
+  if(debug) 
+  {
+    Info<<"   normalMotionSlipPointPatchVectorField::updateCoeffs"<<nl;
+  }
   
   const polyMesh& mesh = this->internalField().mesh()();
   
   const volScalarField& C =
           this->db().objectRegistry::lookupObject<volScalarField>("C");
-  label patchID = this->patch().index();
+  const label& patchID = this->patch().index();
   const IOdictionary& IOd
         = this->db().lookupObject<IOdictionary>("transportProperties");
   scalar lR =  (new dimensionedScalar(IOd.lookup("lR")))->value();
@@ -165,195 +131,8 @@ void Foam::normalMotionSlipPointPatchVectorField::updateCoeffs()
   faceDispl = lR * grC * pNf;
   valuePointPatchField<vector>::updateCoeffs();
 }
-/*
-void Foam::normalMotionSlipPointPatchVectorField::updateCoeffs()
-{
-  Info<<"normalMotionSlip  updateCoeffsp()"<<nl;
  
-  / *
-    tmp<Field<vector>> tvalues =
-        transform(I - n_*n_, this->patchInternalField());
-
-    // Get internal field to insert values into
-    Field<vector>& iF = const_cast<Field<vector>&>(this->primitiveField());
-
-    this->setInInternalField(iF, tvalues());
-   * /
-  
-  
-        const volScalarField& C =
-          this->db().objectRegistry::lookupObject<volScalarField>("C");
-  
-        label patchID = this->patch().index();
-        
-        //const vectorField nHat(this->patch().nf());
-//        const vectorField nHat(mesh.boundaryMesh()[patchID].faceNormals());
-
-        / *
-        const IOdictionary& IOd
-              = this->db().lookupObject<IOdictionary>("transportProperties");
-        
-        scalar lR =  (new dimensionedScalar(IOd.lookup("lR")))->value();
-         * /
-        scalar lR = 0.5;
-//        scalarField grC = -C.boundaryField()[patchID].snGrad();
-
-  const fvMesh& fvmesh_ = refCast<const fvMesh>(mesh);
-  coupledPatchInterpolation patchInterpolator
-  ( 
-    mesh.boundaryMesh()[patchID], fvmesh_
-  );
-
-  //surfaceScalarField ssc = fvc::snGrad(C);
-  
-  scalarField pCf = -C.boundaryField()[patchID].snGrad();
-//  scalarField pCf = -ssc.boundaryField()[patchID];
-  vectorField pNf = mesh.boundaryMesh()[patchID].faceNormals();
-  
-  scalarField motionC    = patchInterpolator.faceToPointInterpolate(pCf);
-  vectorField motionN    = patchInterpolator.faceToPointInterpolate(pNf);
-  
-  // normalize point normals to 1
-  forAll(motionN, ii) motionN[ii]/=mag(motionN[ii]);
-  
-  // set the velocity for the point motion
-  vectorField pointMotion = lR * motionC * motionN;
-  //vectorField pointMotion = lR * pCf * pNf;
-
-        
-//        Info<<"AAAAAAAAA  maxvpointMo= "<<max(pointMotion)<<"   GGG  "<<max(C)<<nl;
-//        Info<<"CCCCCCCCC  maxPrimitiv= "<<max(this->primitiveField())<<nl;
-
-    //tmp<Field<vector>> tvalues =
-    //    ( 
-  //          lR * grC * nHat
-	//					pointMotion
-    //        + 
-  //          transform(I - nHat*nHat, this->patchInternalField()) 
-    //        transform(I - pNf*pNf, this->primitiveField()) 
-    //    );
-            
-    //    transform(I - n_*n_, this->patchInternalField());
-
-    // Get internal field to insert values into
-    Field<vector>& iF = const_cast<Field<vector>&>(this->primitiveField());
-
-    //this->setInInternalField(iF, tvalues());
-   
-		//vectorField pm = patchInterpolator.faceToPointInterpolate( this->primitiveField() );
-		//vectorField pm = patchInterpolator.faceToPointInterpolate( this->patchInternalField() );
-        const volVectorField& cmu =
-          this->db().objectRegistry::lookupObject<volVectorField>("cellMotionU");
-        vectorField cmuB = cmu.boundaryField()[patchID];
-		vectorField pm = patchInterpolator.faceToPointInterpolate( cmuB );
-		//vectorField pm = this->patchInternalField();
-  
-    //tmp<Field<vector>> tvalues =
-    vectorField tvalues =
-        ( 
-  //          lR * grC * nHat
-						pointMotion
-            -
-            transform(I - pNf*pNf, pointMotion) 
-//            + 
-  //          transform(I - nHat*nHat, this->patchInternalField()) 
-//            transform(I - pNf*pNf, pm) 
-        );
-    
-		Info<<"poMo: "<<max(pointMotion)<<"  f0 "<<max(pm)<<"    tval: "<<max(tvalues)<<nl;
-    //this->operator==( this->primitiveField() );
-    //this->operator==( pointMotion );
-    //this->operator==( pm );
-    //this->operator==( tvalues() );
-    this->operator==( tvalues );
-    this->setInInternalField(iF, tvalues);
-    
-		Info<<"IIInternField: "<<max(this->patchInternalField())<<nl;
-        
-valuePointPatchField<vector>::updateCoeffs();
-//fixedValuePointPatchField<vector>::updateCoeffs();
-//calculatedPointPatchField<vector>::updateCoeffs();
-}
-*/
- 
-
-/*
-void Foam::normalMotionSlipPointPatchVectorField::updateCoeffs()
-{
-  if (this->updated())
-  {
-    return;
-  }
-  
-  Info<<"SSSSSSSSSSSSSSSSSSSSS "<<nl;
-  if( this->db().foundObject<IOdictionary>("transportProperties") )
-  {
-    const polyMesh& mesh = this->internalField().mesh()();
-  
-        const volScalarField& C =
-          this->db().objectRegistry::lookupObject<volScalarField>("C");
-  
-        label patchID = this->patch().index();
-        
-        //const vectorField nHat(this->patch().nf());
-        const vectorField nHat(mesh.boundaryMesh()[patchID].faceNormals());
-
-        / *
-        const IOdictionary& IOd
-              = this->db().lookupObject<IOdictionary>("transportProperties");
-        
-        scalar lR =  (new dimensionedScalar(IOd.lookup("lR")))->value();
-         * /
-        scalar lR = 0.5;
-        scalarField grC = -C.boundaryField()[patchID].snGrad();
-        
-        Info<<"AAAAAAAAAAAAAAAAAAAAAA  grC[0]= "<<grC[0]<<nl;
-
-    tmp<Field<vector>> tvalues =
-        ( 
-            lR * grC * nHat
-            + 
-            transform(I - nHat*nHat, this->patchInternalField()) 
-        );
-            
-    //    transform(I - n_*n_, this->patchInternalField());
-
-    // Get internal field to insert values into
-    Field<vector>& iF = const_cast<Field<vector>&>(this->primitiveField());
-
-    this->setInInternalField(iF, tvalues());
-  
-    this->operator==( this->primitiveField() );
-  }
-  fixedValuePointPatchField<vector>::updateCoeffs();
-  
-}
-*/
-
-
-/*
-void Foam::normalMotionSlipPointPatchVectorField::write(Ostream& os) const
-{
-    //slipPointPatchField<vector>::write(os);
-    //calculatedPointPatchField<vector>::write(os);
-    valuePointPatchField<vector>::write(os);
-    this->writeEntry("value", os);
-    //os.writeKeyword("value") << this->patchInternalField() << Foam::token::END_STATEMENT << '\n';
-    //os.writeKeyword("n") << n_ << token::END_STATEMENT << nl;
-}
- */
- 
-
-bool Foam::normalMotionSlipPointPatchVectorField::getIniDisp()
-{
-  return iniDisp;
-}
-
-
-
-
-
-namespace Foam
+  namespace Foam
 {
   makePointPatchTypeField
   (
